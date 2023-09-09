e7db78b916d42e0177f89fa990e772c7c0e6f18d
udeaghad <dozie.udeagha@gmail.com>
2023-09-09 12:05:09 +0100

    Create home page good morning text

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,13 @@ export default function Home() {
       <div className="fixed top-0 w-full bg-white z-50">
         <NavBar />
       </div>
+
+      <section className='px-3 pt-5 mt-16 md:flex md:justify-between md:items-start md:w-full md:px-5'>
+        <div>
+          <h2 className='text-xl font-bold'>Good morning!</h2>
+          <p className='text-sm text-gray-500'>You got some task to do. </p>
+        </div>        
+      </section>
       
     </main>
   )
